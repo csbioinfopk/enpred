9c8589b9c4bf11ada2eba7b560dac62ed6d29c70
csbioinfopk <53048263+csbioinfopk@users.noreply.github.com>
2021-03-18 03:58:32 +0500

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import importlib.util
-
-sys.path.insert(0, "/public_html/enpred/")
-# sys.path.insert(0, os.path.dirname(os.path.realpath('__file__')))
-spec = importlib.util.spec_from_file_location("wsgi", "app.py")
-wsgi = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(wsgi)
-application = wsgi.app
